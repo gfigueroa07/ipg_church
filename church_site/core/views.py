82a7de270aa3ed048936aff9315399fd884e4189
gfigueroa07 <guillermofigueroa2840@gmail.com>
2026-06-09 07:05:14 -0400

    created home.html, home.css and home container

--- church_site/core/views.py
+++ church_site/core/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/home.html')
 
 def donate(request):
     return HttpResponse('donate coming soon...')
