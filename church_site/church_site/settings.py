c95fee1bef293a3dd841e3da531f881ca640fe03
gfigueroa07 <guillermofigueroa2840@gmail.com>
2026-06-18 17:51:08 -0400

    Configure database settings to use dj-database-url for production environment

--- church_site/church_site/settings.py
+++ church_site/church_site/settings.py
@@ -89,11 +89,13 @@ WSGI_APPLICATION = 'church_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 STORAGES = {
